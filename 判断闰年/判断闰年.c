c7f8d4f4bdc3645943a3d57e81e234d26f1b3833
徐铂云 <xuboyun20040417@126.com>
2021-06-25 10:02:53 +0800

    1

---
+++ 判断闰年/判断闰年.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int i;
+	scanf_s("%d", &i);
+	if (i % 400 == 0)
+		printf("是闰年");
+	else if (i % 4 == 0 && i % 100 + !0)
+		printf("是闰年");
+	else
+		printf("不是闰年");
+	return 0;
+}
--- 阶乘/阶乘.c
+++ 阶乘/阶乘.c
@@ -2,7 +2,7 @@
 int main()
 {
 	int i, n, s = 1;
-	scanf("%d", &n);
+	scanf_s("%d", &n);
 	for (i = 1; i <= n; i++)
 		s = s * i;
 	printf("%d", s);
