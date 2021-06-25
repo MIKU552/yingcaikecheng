fb58582a0ef4cc7f8317492bdbe7f06bee3e24ff
徐铂云 <xuboyun20040417@126.com>
2021-06-25 14:41:37 +0800

    14:41

---
+++ 斐波那契数列/斐波那契数列.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a[30];
+	int i;
+	a[0] = 1;
+	a[1] = 1;
+	printf("%d %d ", a[0], a[1]);
+	for (i = 2; i <= 29; i++)
+	{
+		a[i] = a[i - 1] + a[i - 2];
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
