4bea574626915631c8086445513139262775ff6d
徐铂云 <xuboyun20040417@126.com>
2021-06-25 09:52:38 +0800

    添加项目文件。

---
+++ r为3的球的体积和表面积/r为3的球的体积和表面积.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+# define PI 3.1415926
+
+int main()
+{
+	double r, v, s;
+	r = 3;
+	v = 4 * PI * r * r * r / 3;
+	s = 4 * PI * r * r;
+	printf("v=%lf\ts=%lf\n", v, s);
+	return 0;
+}
---
+++ 阶乘/阶乘.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, n, s = 1;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		s = s * i;
+	printf("%d", s);
+	return 0;
+}
