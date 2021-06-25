5eca250418a774ca96f06f680c0a1e50b4c1a085
徐铂云 <xuboyun20040417@126.com>
2021-06-25 14:23:31 +0800

    14:23

---
+++ 百钱买百鸡/百钱买百鸡.c
@@ -0,0 +1,17 @@
+﻿//公鸡5元 母鸡3元 小鸡一元三个
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	for (a = 0; a <= 20; a++)
+	{
+		for (b = 0; b < 34; b++)
+		{
+			for (c = 0; c <= 99; c = c + 3)
+			{
+				if (5 * a + 3 * b + c / 3 == 100 && a + b + c == 100)	printf("公鸡%d只\n母鸡%d只\n小鸡%d只\n\n", a, b, c);
+			}
+		}
+	}
+	return 0;
+}
