9dd0b828acf4984fdaa789d98c06fcb735e47bc2
徐铂云 <xuboyun20040417@126.com>
2021-06-26 08:51:11 +0800

    阶乘（自定义函数）

--- 阶乘/阶乘.c
+++ 阶乘/阶乘.c
@@ -1,10 +1,30 @@
 ﻿#include<stdio.h>
+
+//一般解法
+//int main()
+//{
+//	int i, n, s = 1;
+//	scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++)
+//		s = s * i;
+//	printf("%d", s);
+//	return 0;
+//}
+
+//自定义函数解法
+int factorial(int n)
+{
+	int i,result = 1;
+	for (i = 1;i <= n;i++)
+	{
+		result *= i;
+	}
+	return result;
+}
 int main()
 {
-	int i, n, s = 1;
+	int n;
 	scanf_s("%d", &n);
-	for (i = 1; i <= n; i++)
-		s = s * i;
-	printf("%d", s);
+	printf("%d", factorial(n));
 	return 0;
 }
