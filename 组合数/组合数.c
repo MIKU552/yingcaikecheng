6c548f192604042a55fd4a6ae4a8f59eb6f91114
徐铂云 <xuboyun20040417@126.com>
2021-06-26 09:24:38 +0800

    组合数

---
+++ 组合数/组合数.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+int factorial(int n)
+{
+	int i, result = 1;
+	for (i = 1; i <= n; i++)	result *= i;
+	return result;
+}
+int combinatorial(int n, int m)
+{
+	int result;
+	result = factorial(n) / factorial(m) / factorial(n - m);
+	return result;
+}
+int main()
+{
+	int n, m;
+	scanf_s("%d%d", &n, &m);
+	printf("%d", combinatorial(n, m));
+	return 0;
+}
